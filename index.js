2afcb6fc1672142d0194d982bdec21e706352895
Cameron O'Rourke <cameron.orourke@oracle.com>
2016-03-11 23:38:02 -0800

    Adding DB to save users

--- index.js
+++ index.js
@@ -2,13 +2,10 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var Datastore = require('nedb')
+    , db = new Datastore({ filename: 'db/hackwarzdata.nedb', autoload: true });
 
-
-
-var usermap = new Map();
-
-var password = "minty33"
-
+// This just serves up the web game UI
 app.get('/', function(req, res){
     res.sendFile(__dirname + '/index.html');
 });
@@ -19,44 +16,70 @@ io.on('connection', function(socket){
 
     // A new user has connected
     var folder = "hacks, firewall, money";
+    user = {
+        name: "",
+        password: "",
+        level: 0,
+        bitcoins: 0
+    };
 
-    var username = "";
-    var score = 0; // The gamer's score
-    var budget = 0; // The gamer's 'security budget'
     console.log('A user connected. Socket #' + socket.id);
     socket.emit('chat_message', 'SYSTEM: Welcome to Mega Corp. Please login using /user [name]');
 
+    // Repeating function to count money, etc.
     setInterval(function() {
-        score += 10;
-        socket.emit('score', score);
+        user.bitcoins += user.level*10;
+        socket.emit('bitcoins', user.bitcoins);
+        socket.emit('level', user.level);
     }, 1000);
 
+    // Save the user information
     socket.on('disconnect', function(){
-        console.log('User disconnected: ' + (username == "" ? socket.id : username));
+        // TODO: check to see if record already created and update else insert
+        if(user.name != "") {
+            if(user._id) {
+                console.log ("Updating user info in DB");
+                db.update({_id: user._id}, user, {}, function(err, numReplaced){} );
+            } else {
+                console.log("Writing user info to DB");
+                db.insert(user, function (err, newDoc) {
+                    console.log("DB insert: " + err);
+                });
+            }
+        }
+        console.log('User disconnected: ' + (user.name == "" ? socket.id : user.name));
     });
 
     socket.on('chat_message', function(msg){
+
         // Don't allow user to continue until they identify themselves
-        if(username == "" && msg.startsWith("/name") == false) {
-            socket.emit("chat_message", "SYSTEM: Please login.");
-            return;
+        if(user.name == "") {
+            // But also don't allow use of /name command after login
+            if (msg.startsWith('/name ') || msg.startsWith('/user ')) {
+                var u = (msg.substring(msg.indexOf(" ")+1));
+                // Look up user in db, if found set the user object
+                db.find({name: u}, function(err, docs) {
+                    console.log(docs);
+                    if(docs.length > 0) {
+                        // TODO: don't use docs.count its a hack
+                        user = docs[docs.length - 1];
+                        socket.emit('chat_message', "SYSTEM: Welcome back.");
+                    } else {
+                        console.log("No user found in DB");
+                        user.name = u;
+                    }
+                }); // end db.find
+                socket.emit('chat_message', "SYSTEM: Login successful.");
+                user.level++;
+                return;
+            } else {
+                socket.emit('chat_message', "Please login.");
+                return;
+            }
         }
-
-
 
         // is the message a command?
         if(msg.startsWith('/')) {
-            // is it the /name command?
-            // The string will be: /name username
-            if(msg.startsWith('/name ')) {
-                username = (msg.substring(msg.indexOf(" ")));
-                socket.emit('chat_message', "SYSTEM: Login successful.");
-            }
-            // Is it the /score command?
-            // TODO: Don't require a score command -- just update the score periodically
-            if(msg == "/score") {
-                io.emit('score', score);
-            }
 
             if (msg.startsWith('/color')){
 
@@ -65,19 +88,16 @@ io.on('connection', function(socket){
             if(msg.startsWith('/help')) {
                 socket.emit('chat_message', "Staff Help" + ': ' + "1) /name:" +
                     " assign your name after typing /name ");
-                socket.emit('chat_message', "Staff Help" + ': ' + "2)/score:" +
-                    " type /score and hit return to get your score");
             }
 
             if(msg.startsWith('/folder')){
                 socket.emit('chat_message', username + "'s" + "folder" + folder);
 
-
             }
 
         } else {
             //Its not a command, just send out the message to everyone
-            io.emit('chat_message', username + ': ' + msg);
+            io.emit('chat_message', user.name + ': ' + msg);
         }
 
     });

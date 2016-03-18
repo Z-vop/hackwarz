0c26f61ce4810b2dd11db43f6a9e3b2b5bf9b972
Oliver <oliver.c.orourke@gmail.com>
2016-03-17 18:39:14 -0700

    Starting missions / quests code

--- index.js
+++ index.js
@@ -10,9 +10,17 @@ app.get('/', function(req, res){
     res.sendFile(__dirname + '/index.html');
 });
 
+var mission = 0;
+
 
 
 io.on('connection', function(socket){
+
+    function write(text){
+        socket.emit('chat_message', text);
+    }
+
+
 
     // A new user has connected
     var folder = "hacks, firewall, money";
@@ -24,13 +32,20 @@ io.on('connection', function(socket){
     };
 
     console.log('A user connected. Socket #' + socket.id);
-    socket.emit('chat_message', 'SYSTEM: Welcome to Mega Corp. Please login using /user [name]');
+    socket.emit('chat_message', 'SYSTEM: Welcome to the DeepWeb Network. Please login using /user [name]');
 
     // Repeating function to count money, etc.
     setInterval(function() {
+        /* Update bitcoins, user level */
         user.bitcoins += (1.8^user.level)/100;
         socket.emit('bitcoins', user.bitcoins);
         socket.emit('level', user.level);
+
+        /* Check missions */
+        if (mission == 2) {
+            mission++;
+            write("Well?? Get going already!!");
+        };
     }, 1000);
 
     // Save the user information
@@ -56,7 +71,8 @@ io.on('connection', function(socket){
             // But also don't allow use of /name command after login
             if (msg.startsWith('/name ') || msg.startsWith('/user ')) {
                 var u = (msg.substring(msg.indexOf(" ")+1));
-                var patt = /[a-z][0-9]/;
+                /* Username must 2start with letter, and be one or more letters and numbers */
+                var patt = /[a-z0-9]+/;
                 if(!patt.test(u)) {
                     socket.emit('error_message', "invalid name request");
                     return;
@@ -69,13 +85,16 @@ io.on('connection', function(socket){
                         // TODO: don't use docs.count its a hack
                         user = docs[docs.length - 1];
                         socket.emit('chat_message', "SYSTEM: Welcome back.");
+                        mission++;
                     } else {
+                        /* A new user name */
                         console.log("No user found in DB");
                         user.name = u;
+                        user.level++;
+                        mission++;
                     }
                 }); // end db.find
                 socket.emit('chat_message', "SYSTEM: Login successful.");
-                user.level++;
                 return;
             } else {
                 socket.emit('chat_message', "Please login.");
@@ -102,9 +121,33 @@ io.on('connection', function(socket){
 
         } else {
             //Its not a command, just send out the message to everyone
-            io.emit('chat_message', user.name + ': ' + msg);
+            socket.emit('chat_message', user.name + ': ' + msg);
         }
 
+        if (mission == 1){
+            mission++;
+
+            setTimeout(write("Incoming Message from: unknown"),1500)
+
+            setTimeout(mission2text, 2000);
+
+            function mission2text(){
+                write("Message: " +
+                    "Ah good i see your here," +
+                    "look i have a task i need some help on," +
+                    " i need you to break into some guys network and get his password," +
+                    " think your up for the task?");
+
+                if (msg){
+                    setTimeout(function(){write("alright sweet," +
+                        " i will give a few tips on how to hack his network");
+                    },6500);
+
+                }
+            }
+
+
+        }
     });
 });
 
